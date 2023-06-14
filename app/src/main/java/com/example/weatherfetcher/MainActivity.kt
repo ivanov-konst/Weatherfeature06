3a879be6a41acf77cf8c3d62b0bd283a9ad3aeed
Konstantin <ivanov-konst@list>
2023-06-14 22:04:21 +0300

    Температура + ветер

---
+++ app/src/main/java/com/example/weatherfetcher/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.weatherfetcher
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherfetcher.feature.SecondActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val etCity: EditText by lazy { findViewById(R.id.etCity) }
+    private val btn: Button by lazy { findViewById(R.id.button) }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btn.setOnClickListener {
+            val city = etCity.text.toString()
+            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            Intent(intent)
+            intent.putExtra("city", city)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/weatherfetcher/feature/constans.kt
@@ -0,0 +1,4 @@
+package com.example.weatherfetcher.feature
+
+const val API_KEY ="2dabb7bff44f2f4d4bb7ce52ebc6f134"
+const val BASE_URL ="https://api.openweathermap.org/data/2.5/"
